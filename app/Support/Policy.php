9890a4af780c408e185b87b0b7be83e7e5e683f1
Алексей Цапаев <a.tsapaev@think24.ru>
2023-07-03 20:25:56 +0300

    мс

--- app/Support/Policy.php
+++ app/Support/Policy.php
@@ -4,101 +4,47 @@ namespace App\Support;
 
 use Spatie\Csp\Directive;
 use Spatie\Csp\Policies\Policy as BasePolicy;
-use Spatie\Csp\Scheme;
+use Spatie\Csp\Keyword;
 
 class Policy extends BasePolicy
 {
     public function configure()
     {
         $this
-            ->addGeneralDirectives()
-            ->addDirectivesForCarbon()
-            ->addDirectivesForGoogleFonts()
-            ->addDirectivesForGoogleAnalytics()
-            ->addDirectivesForGoogleTagManager()
-            ->addDirectivesForTwitter()
-            ->addDirectivesForYouTube()
-            ->addDirective(Directive::STYLE, 'self')
-            ->addDirective(Directive::SCRIPT, 'self')
-            ->addDirective(Directive::FONT, 'self');
-    }
-
-    protected function addGeneralDirectives(): self
-    {
-        return $this
-            ->addDirective(Directive::BASE, 'self')
-            ->addNonceForDirective(Directive::SCRIPT)
+            ->addDirective(Directive::BASE, Keyword::SELF)
+            ->addDirective(Directive::CONNECT, Keyword::SELF)
+            ->addDirective(Directive::IMG, Keyword::SELF)
+            ->addDirective(Directive::STYLE, Keyword::SELF)
+            ->addDirective(Directive::FORM_ACTION, Keyword::SELF)
+            ->addDirective(Directive::IMG, [
+                Keyword::SELF,
+                'data:',
+                'blob:'
+            ])
+            ->addDirective(Directive::FONT, [
+                Keyword::SELF,
+                'data:',
+                'blob:'
+            ])
             ->addDirective(Directive::SCRIPT, [
-                'murze.be',
-                'murze.be.test',
+                Keyword::SELF,
+                'data:',
+                'blob:'
             ])
             ->addDirective(Directive::STYLE, [
-                'murze.be',
-                'murze.be.test',
-                'unsafe-inline',
-            ])
-            ->addDirective(Directive::FORM_ACTION, [
-                'murze.be',
-                'murze.be.test',
-                'sendy.murze.be',
-            ])
-            ->addDirective(Directive::IMG, [
-                '*',
-                'unsafe-inline',
+                Keyword::SELF,
                 'data:',
+                'blob:'
             ])
-            ->addDirective(Directive::OBJECT, 'none');
-    }
-
-    protected function addDirectivesForCarbon(): self
-    {
-        return $this->addDirective(Directive::SCRIPT, [
-            'srv.carbonads.net',
-            'script.carbonads.com',
-            'cdn.carbonads.com',
-        ]);
-    }
-
-    protected function addDirectivesForGoogleFonts(): self
-    {
-        return $this
+            ->addDirective(Directive::MEDIA, Keyword::SELF)
+            ->addDirective(Directive::SCRIPT, [
+                Keyword::SELF,
+                'www.google.com',
+                'www.gstatic.com'
+            ])
+            ->addDirective(Directive::OBJECT, Keyword::NONE)
             ->addDirective(Directive::FONT, 'fonts.gstatic.com')
             ->addDirective(Directive::SCRIPT, 'fonts.googleapis.com')
             ->addDirective(Directive::STYLE, 'fonts.googleapis.com');
-    }
-
-    protected function addDirectivesForGoogleAnalytics(): self
-    {
-        return $this->addDirective(Directive::SCRIPT, '*.google-analytics.com');
-    }
-
-    protected function addDirectivesForGoogleTagManager(): self
-    {
-        return $this->addDirective(Directive::SCRIPT, '*.googletagmanager.com');
-    }
-
-    protected function addDirectivesForTwitter(): self
-    {
-        return $this
-            ->addDirective(Directive::SCRIPT, [
-                'platform.twitter.com',
-                '*.twimg.com',
-            ])
-            ->addDirective(Directive::STYLE, [
-                'platform.twitter.com',
-            ])
-            ->addDirective(Directive::FRAME, [
-                'platform.twitter.com',
-                'syndication.twitter.com',
-            ])
-            ->addDirective(Directive::FORM_ACTION, [
-                'platform.twitter.com',
-                'syndication.twitter.com',
-            ]);
-    }
-
-    protected function addDirectivesForYouTube(): self
-    {
-        return $this->addDirective(Directive::FRAME, '*.youtube.com');
     }
 }
