977014c8aade070de32c4cdcc50b27106fb21790
Rets Dmitriy <retsdmitriy@gmail.com>
2024-03-11 22:09:47 +0500

    temp start

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,10 +4,10 @@
 <head>
   <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="anonymous">
   <link rel="preload" as="style" onload="this.rel='stylesheet'" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap">
-  <link rel="preload" as="font" type="font/eot" href="/client/admin/eot/materialdesignicons-webfont-kq_ClZaA.eot" crossorigin="anonymous">
-  <link rel="preload" as="font" type="font/woff2" href="/client/admin/woff2/materialdesignicons-webfont-6eb_lmTU.woff2" crossorigin="anonymous">
-  <link rel="preload" as="font" type="font/woff" href="/client/admin/woff/materialdesignicons-webfont-D15t_tsC.woff" crossorigin="anonymous">
-  <link rel="preload" as="font" type="font/ttf" href="/client/admin/ttf/materialdesignicons-webfont-e5j8FT_3.ttf" crossorigin="anonymous">
+  <link rel="preload" as="font" type="font/eot" href="/client/admin/eot/materialdesignicons-webfont-CSr8KVlo.eot" crossorigin="anonymous">
+  <link rel="preload" as="font" type="font/woff2" href="/client/admin/woff2/materialdesignicons-webfont-Dp5v-WZN.woff2" crossorigin="anonymous">
+  <link rel="preload" as="font" type="font/woff" href="/client/admin/woff/materialdesignicons-webfont-PXm3-2wK.woff" crossorigin="anonymous">
+  <link rel="preload" as="font" type="font/ttf" href="/client/admin/ttf/materialdesignicons-webfont-B7mPwVP_.ttf" crossorigin="anonymous">
 
     <meta charset="UTF-8" />
     <link rel="icon" href="/favicon.ico" />
@@ -16,7 +16,7 @@
   <script type="module" crossorigin src="/client/admin/js/index.js"></script>
   <link rel="modulepreload" crossorigin href="/client/admin/js/vue-codemirror.js">
   <link rel="modulepreload" crossorigin href="/client/admin/js/lodash.js">
-  <link rel="stylesheet" crossorigin href="/client/admin/css/index-pw96n5U_.css">
+  <link rel="stylesheet" crossorigin href="/client/admin/css/index-DAiuhRQw.css">
 </head>
 
 <body>
